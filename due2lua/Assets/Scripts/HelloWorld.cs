c4f2a0612e1a2395ad27966731643a2983efc00e
bettez1998 <zhujunjie0812@gmail.com>
2020-10-01 00:41:23 +0800

    due2lua

---
+++ due2lua/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Start()
+    {
+        LuaEnv luaEnv = new LuaEnv();
+        luaEnv.DoString("print('Hello World')");
+    }
+
+}
